978eab5a8ef1beb511d9ea4e970e532f9cdb64bc
knekill <nr366@cornell.edu>
2015-03-21 19:36:46 -0400

    Added viewDaemon

---
+++ LSI-Project1b/src/View/viewDaemon.java
@@ -0,0 +1,48 @@
+package View;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class viewDaemon  implements Runnable{
+	
+	HashMap<String, String> ServerView;
+	
+	@Override
+	public void run(){
+		Double numProbability = 0.0;
+		Double randomProbability = 0.0;
+		while(true){
+			try {
+				numProbability = (double) (1/ServerView.size());
+				Random random = new Random();
+				randomProbability = random.nextDouble();
+				
+				if (numProbability > randomProbability) {
+					
+					Set<String> serverList = ServerView.keySet();
+					java.util.Collections.shuffle((List<?>) serverList);
+					Iterator serverIterator = serverList.iterator();
+					String chosenServer = (String) serverIterator.next();
+					
+//					RPC to chosen server
+//					exchange views
+//					ServerView.mergeViews(local,exchangedView);
+					
+				} else {
+//					SimpleDB shenanigans
+					
+				}
+				Thread.sleep(new Random().nextLong() * 10000);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+}
