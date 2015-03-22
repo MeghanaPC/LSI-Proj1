6b15628e6bf7a77dd28a07525b09fda123177dd9
knekill <nr366@cornell.edu>
2015-03-21 20:48:33 -0400

    ServerView files moved out Added HashMapToString method

--- LSI-Project1b/src/View/ServerView.java
+++ LSI-Project1b/src/View/ServerView.java
@@ -24,50 +24,6 @@ public class ServerView {
 		serverView=view;
 	}
 	
-//	for debugging purposes
-	public void iterateHashMap(ConcurrentHashMap<String, String> hMap){
-		
-		for(String key:hMap.keySet()){
-			System.out.println(key + " " + hMap.get(key));
-		}
-	}
-	
-//	Merges two views based on latest timestamp policy 
-	public ConcurrentHashMap<String, String> mergeViews(ConcurrentHashMap<String, String> mapA, ConcurrentHashMap<String, String> mapB) {
-		
-		ConcurrentHashMap<String, String> mapM = new ConcurrentHashMap<String, String>();
-		Set<String> keysetA = mapA.keySet();
-		Set<String> keysetB = mapB.keySet();
-		
-		for(String keyA:keysetA){
-			if (!mapB.containsKey(keyA)) {
-				mapM.put(keyA, mapA.get(keyA));
-			}
-			else {
-				String parseValueA = mapA.get(keyA);
-				String parseValueB = mapB.get(keyA);
-				
-				String[] splitValueA = parseValueA.split("_");
-				String[] splitValueB = parseValueB.split("_");
-				
-				if (Long.parseLong(splitValueA[1]) >= Long.parseLong(splitValueB[1])) {
-					mapM.put(keyA, parseValueA);
-				}
-				else {
-					mapM.put(keyA, parseValueB);
-				}
-				
-			}
-		}
-	
-		for(String keyB:keysetB){
-			if (!mapA.containsKey(keyB)) {
-				mapM.put(keyB, mapB.get(keyB));
-			}
-		}	
-		return mapM;
-	}
-	
 //	Tested and working
 	/*
 	public static void main(String[] args){
--- LSI-Project1b/src/lsi/extraUtils.java
+++ LSI-Project1b/src/lsi/extraUtils.java
@@ -1,6 +1,7 @@
 package lsi;
 
 import java.util.HashMap;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.swing.text.StyledEditorKit.ForegroundAction;
@@ -28,20 +29,68 @@ public class extraUtils {
 	
 	public static String hashMapToString(ConcurrentHashMap<String, String> map){
 		
-		String result = "blah";
-		//StringBuilder
+		StringBuilder resultBuilder = new StringBuilder();
+		Set<String> resultKeySet = map.keySet();
+		int length = 0;
 		
-		return result;
+		for(String key:resultKeySet){
+			resultBuilder.append(key+DELIMITER_LEVEL2+map.get(key)+DELIMITER_LEVEL1);
+		}
+		
+		length = resultBuilder.length();
+		return resultBuilder.toString().substring(0, length-1);
 	}
 	
+	public static void iterateHashMap(ConcurrentHashMap<String, String> hMap){
+		
+		for(String key:hMap.keySet()){
+			System.out.println(key + " " + hMap.get(key));
+		}
+	}
+	
+	public static ConcurrentHashMap<String, String> mergeViews(ConcurrentHashMap<String, String> mapA, ConcurrentHashMap<String, String> mapB) {
+		
+		ConcurrentHashMap<String, String> mapM = new ConcurrentHashMap<String, String>();
+		Set<String> keysetA = mapA.keySet();
+		Set<String> keysetB = mapB.keySet();
+		
+		for(String keyA:keysetA){
+			if (!mapB.containsKey(keyA)) {
+				mapM.put(keyA, mapA.get(keyA));
+			}
+			else {
+				String parseValueA = mapA.get(keyA);
+				String parseValueB = mapB.get(keyA);
+				
+				String[] splitValueA = parseValueA.split("_");
+				String[] splitValueB = parseValueB.split("_");
+				
+				if (Long.parseLong(splitValueA[1]) >= Long.parseLong(splitValueB[1])) {
+					mapM.put(keyA, parseValueA);
+				}
+				else {
+					mapM.put(keyA, parseValueB);
+				}
+				
+			}
+		}
+	
+		for(String keyB:keysetB){
+			if (!mapA.containsKey(keyB)) {
+				mapM.put(keyB, mapB.get(keyB));
+			}
+		}	
+		return mapM;
+	}
+	
+	
 //	For testing purposes
-	/*
 	public static void main(String[] args) {
 		
-		extraUtils eutils = new extraUtils();
-		ConcurrentHashMap<String, String> demoMap = eutils.stringToHashMap("server1_up_123456-server2_down_345678");
-		View.ServerView testView = new View.ServerView();
-		testView.iterateHashMap(demoMap);
+		ConcurrentHashMap<String, String> demoMap = stringToHashMap("server1_up_123456-server2_down_345678");
+//		iterateHashMap(demoMap);
+		
+		System.out.println(hashMapToString(demoMap));
 	}
-	*/ 
+	 
 }
