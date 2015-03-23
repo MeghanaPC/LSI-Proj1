ec432896de5444e4b0e6618f1dc8fb28d0e96d2d
meghana <mp872@cornell.edu>
2015-03-23 17:42:09 -0400

    Simple DB methods

--- LSI-Project1b/src/View/SimpleDbAccess.java
+++ LSI-Project1b/src/View/SimpleDbAccess.java
@@ -1,49 +1,140 @@
 package View;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 import lsi.ViewManager;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.simpledb.AmazonSimpleDBClient;
 import com.amazonaws.services.simpledb.model.*;
 import com.amazonaws.auth.BasicAWSCredentials;
 
 public class SimpleDbAccess {
 
-    public static AmazonSimpleDBClient sdb;
-    public static String DOMAIN_NAME = "LSIProj1b";
-    public static String ITEM_NAME = "ViewItem";
+	public static AmazonSimpleDBClient sdb;
+	public static final String DOMAIN_NAME = "LSIProj1b";
+	public static final String ITEM_NAME = "ViewItem";
+	public static final String ATTR_NAME = "ATTR";
 
+	// returns true if the domain was created
+	public static boolean createSimpleDbDomainIfNotExists() {
 
-	public static void gossipWithSimpleDb(){
-		
-		if(sdb == null){
-			sdb = new AmazonSimpleDBClient(new BasicAWSCredentials("AKIAIXCNDMOAWSH7JEJA", "EoZEMMGkTgRcYVwn3hUPdz7BRkSXhSUuaczQkOZU"));
+		ArrayList<String> existingDomains = new ArrayList<String>();
+		String nextToken = null;
+		do {
+			ListDomainsRequest ldr = new ListDomainsRequest();
+			ldr.setNextToken(nextToken);
+
+			ListDomainsResult result = sdb.listDomains(ldr);
+			existingDomains.addAll(result.getDomainNames());
+
+			nextToken = result.getNextToken();
+		} while (nextToken != null);
+
+		if (!existingDomains.contains(DOMAIN_NAME)) {
+			CreateDomainRequest crd = new CreateDomainRequest();
+			crd.setDomainName(DOMAIN_NAME);
+			try {
+				sdb.createDomain(crd);
+			} catch (Exception e) {
+				return false;
+			}
+			return true;
 		}
-		ConcurrentHashMap<String, String> dbView = getViewFromDB();
-		if(dbView != null ){
-			//mergeViews() DB view with self view
-			ConcurrentHashMap<String, String> mergedView = ViewManager.mergeViews(dbView, ServerView.serverView);
-			//Send DB the mergedView
-			putViewToDB(mergedView);
-			
+		return false;
+
+	}
+
+	public static void gossipWithSimpleDb() {
+
+		String dbViewString = null;
+
+		if (sdb == null) {
+			sdb = new AmazonSimpleDBClient(new BasicAWSCredentials(
+					"AKIAIXCNDMOAWSH7JEJA",
+					"EoZEMMGkTgRcYVwn3hUPdz7BRkSXhSUuaczQkOZU"));
+		}
+		try {
+			dbViewString = getViewFromDB();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		if (dbViewString != null) {
+
+			ConcurrentHashMap<String, String> dbView = ViewManager
+					.stringToHashMap(dbViewString);
+			// mergeViews() DB view with self view
+			ConcurrentHashMap<String, String> mergedView = ViewManager
+					.mergeViews(dbView, ServerView.serverView);
+			// Send DB the mergedView
+			String mergedViewString = ViewManager.hashMapToString(mergedView);
+
+			putViewToDB(dbViewString, mergedViewString);
+
 			ViewManager.mergeViewWithSelf(mergedView);
-			//mergeWithSelf(mergedView)
+			// mergeWithSelf(mergedView)
+
+		} else {
+			// Assuming that DB is empty as no server has yet merged its view
+			// with DB
+			String serverViewString = ViewManager
+					.hashMapToString(ServerView.serverView);
+			putFirstTimeViewToDB(serverViewString);
 		}
-		
+
 	}
 
-	private static void putViewToDB(ConcurrentHashMap<String, String> mergedView) {
+	private static void putFirstTimeViewToDB(String serverViewString) {
 
-		
+		PutAttributesRequest putAttributesRequest = new PutAttributesRequest();
+		putAttributesRequest.setDomainName(DOMAIN_NAME);
+		List<ReplaceableAttribute> data = new ArrayList<ReplaceableAttribute>();
+		data.add((new ReplaceableAttribute().withName(ATTR_NAME)
+				.withValue(serverViewString)));
+		putAttributesRequest.setItemName(ITEM_NAME);
+		putAttributesRequest.setAttributes(data);
+		try {
+			sdb.putAttributes(putAttributesRequest);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	private static ConcurrentHashMap<String, String> getViewFromDB() {
-		
+	private static void putViewToDB(String oldDbView, String updatedDbView) {
+
+		PutAttributesRequest putAttributesRequest = new PutAttributesRequest();
+		putAttributesRequest.setDomainName(DOMAIN_NAME);
+		List<ReplaceableAttribute> data = new ArrayList<ReplaceableAttribute>();
+		data.add((new ReplaceableAttribute().withName(ATTR_NAME)
+				.withValue(updatedDbView)));
+		putAttributesRequest.setItemName(ITEM_NAME);
+		putAttributesRequest.setAttributes(data);
+		UpdateCondition condition = new UpdateCondition(ATTR_NAME, oldDbView,
+				true);
+		putAttributesRequest.setExpected(condition);
+		try {
+			sdb.putAttributes(putAttributesRequest);
+		} catch (AmazonServiceException e) {
+			if (e.getErrorCode().equals("409")) {
+				// retry
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static String getViewFromDB() throws Exception {
+
 		SelectResult result = null;
 		SelectRequest sr = new SelectRequest("select * from " + DOMAIN_NAME,
 				Boolean.TRUE);
+
 		result = sdb.select(sr);
 
 		for (Item item : result.getItems()) {
@@ -52,7 +143,7 @@ public class SimpleDbAccess {
 			if (s.equals(ITEM_NAME)) {
 				List<Attribute> attr = item.getAttributes();
 				String viewString = attr.get(0).getValue();
-				return (ViewManager.stringToHashMap(viewString));
+				return viewString;
 
 			}
 		}
